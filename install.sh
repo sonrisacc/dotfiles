c24632bf00b8ea2381f7cb8640fc862f35ff3588
sonrisacc <sonrisa722611@gmail.com>
2022-05-23 14:53:22 -0700

    Test

--- install.sh
+++ install.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "                 cloning zsh-zsh-nvm                       "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+}
 
-brew install wget
-brew install git
-brew install macvim
-brew install lazygit
+lazygit() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    mkdir -p ~/bin
+    cd ~/bin
+    git clone https://github.com/jesseduffield/lazygit.git
+    cd lazygit
+    go install
+
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    echo "alias lg='lazygit'" >> ~/.zshrc
+}
+
+zshrc
+lazygit
